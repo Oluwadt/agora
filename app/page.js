b30a7de482f050ac0c133b324b282e484be0d542
Oluwadt <dtonilede@gmail.com>
2025-12-12 16:20:13 +0100

    Added ProductCard component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <div>
-        Welcome to Agora
-    </div>    
-  );
-}
+    import ProductCard from "./components/ProductCard";
+
+    export default async function Home() {
+
+        const products = await fetch('https://dummyjson.com/products')
+        .then(res => res.json())
+        .then(products_json => products_json['products']);
+
+        return (
+            <div>
+                Welcome to Agora
+                <ProductCard products={products} />
+            </div>    
+        );
+    }
